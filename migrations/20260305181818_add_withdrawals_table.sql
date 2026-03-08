3892e87e9c89eaa28cce1ba63cfa57eb12b81e76
Ilia Onishchenko <sinoayli@gmail.com>
2026-03-08 02:19:47 +0200

    remove constaint on orders in withdrawals table

--- migrations/20260305181818_add_withdrawals_table.sql
+++ migrations/20260305181818_add_withdrawals_table.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 CREATE TABLE withdrawals(
-    order_number VARCHAR(255) PRIMARY KEY REFERENCES orders(number),
+    order_number VARCHAR(255) PRIMARY KEY,
     user_id UUID NOT NULL REFERENCES users(id),
     sum NUMERIC,
     processed_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
